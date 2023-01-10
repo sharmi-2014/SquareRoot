1fdd8bfeddcb77e62a8e8be5c783211f0ec6b9a3
sharmi-2014 <121440788+sharmi-2014@users.noreply.github.com>
2023-01-10 17:11:26 +0530

    Add files via upload

---
+++ SquareRoot.java
@@ -0,0 +1,20 @@
+package javachallenge;
+
+public class SquareRoot {
+public static void main(String[] args) {
+	
+	int num=4;
+	for( int i=0;i<=num/2;i++) {
+	if(i*i==num) {
+		System.out.println(i+ "is the square root i");
+	}
+	}
+	int num1=8;
+	for(int j=0;j<=num1/2;j++) {
+		if(j*j==num1/2) {
+			System.out.println(j+ " is the square root of j");
+		}
+	}
+		
+	}
+}
